3a4d536ad553a700f464b170a8854f017ccdb726
Kiyalbek kyzy Jasmina <jasmina.kiyalbekkyzy@alatoo.edu.kg>
2026-04-24 09:44:03 +0600

    add frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,38 +1,38 @@
-const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = "https://railway-demo-production-8ae2.up.railway.app/api/data";
 
-async function load() {
-  const res = await fetch(API + "/api/data");
-  const data = await res.json();
+async function loadItems() {
+    const res = await fetch(API_URL);
+    const data = await res.json();
 
-  const list = document.getElementById("list");
-  list.innerHTML = "";
+    const list = document.getElementById("list");
+    list.innerHTML = "";
 
-  data.forEach(item => {
-    const li = document.createElement("li");
-    li.innerText = item[1];
-
-    const btn = document.createElement("button");
-    btn.innerText = "Delete";
-    btn.onclick = () => del(item[0]);
-
-    li.appendChild(btn);
-    list.appendChild(li);
-  });
+    data.forEach(item => {
+        const li = document.createElement("li");
+        li.innerHTML = item.name + 
+        ` <button onclick="deleteItem(${item.id})">Delete</button>`;
+        list.appendChild(li);
+    });
 }
 
-async function add() {
-  const value = document.getElementById("input").value;
-  await fetch(API + "/api/data", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({name: value})
-  });
-  load();
+async function addItem() {
+    const name = document.getElementById("name").value;
+
+    await fetch(API_URL, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({name})
+    });
+
+    loadItems();
 }
 
-async function del(id) {
-  await fetch(API + "/api/data/" + id, { method: "DELETE" });
-  load();
+async function deleteItem(id) {
+    await fetch(`${API_URL}/${id}`, {
+        method: "DELETE"
+    });
+
+    loadItems();
 }
 
-load();
+loadItems();
